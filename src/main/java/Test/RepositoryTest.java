821eecdeb96b92dc23d3abd89e5bac91e359513b
Dmitry Karebin <xarder1@gmail.com>
2018-11-27 01:15:30 +0300

    add:RepositoryTest

---
+++ src/main/java/Test/RepositoryTest.java
@@ -0,0 +1,41 @@
+package Test;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepositoryTest {
+
+    @Test
+    void removePerson() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getPerson() {
+    }
+
+    @Test
+    void getAgeOfPerson() {
+    }
+
+    @Test
+    void sortBy() {
+    }
+
+    @Test
+    void searchByFio() {
+    }
+
+    @Test
+    void searchByGender() {
+    }
+
+    @Test
+    void searchByDateOfBirth() {
+    }
+}
